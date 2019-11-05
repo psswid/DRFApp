f08f0969d819767663c8d0b7beb47097ed8932f3
Piotr Świderski <psswiderski@gmail.com>
2019-11-05 17:05:09 +0100

    celery comments counter

--- api/apps/blog/views.py
+++ api/apps/blog/views.py
@@ -3,7 +3,6 @@ from rest_framework.permissions import (AllowAny, IsAdminUser)
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
-from .tasks import count_comments
 from .models import Entry
 from .documents import EntryDocument
 from .renderers import EntryJSONRenderer
